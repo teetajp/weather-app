923e491416e4dc06578589d63019be890ff52186
TJ Pavaritpong <teetajp@gmail.com>
2022-06-21 11:35:31 +0700

    fix css

--- src/features/weather/WeatherCard.js
+++ src/features/weather/WeatherCard.js
@@ -11,18 +11,14 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { selectAllWeatherData, weatherRemoved } from './weatherSlice';
 
-import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
 
-let theme = createTheme();
-theme = responsiveFontSizes(theme);
 
 export default function WeatherDisplay () {
     const weatherData = useSelector(selectAllWeatherData);
     
     return (
-        <Grid container spacing={4} justifyContent="center" alignItems="stretch" sx={{ padding: "10px"}}>
+        <Grid container spacing={4} justifyContent="center" sx={{ padding: "5px"}}>
             {weatherData.map(weatherLocation => (
-                // TODO: set xs and md and lg
                 <Grid item key={weatherLocation.id}>
                     <WeatherCard {...weatherLocation}/>
                 </Grid>
@@ -35,22 +31,20 @@ export default function WeatherDisplay () {
 const WeatherCard = ({ id="", city="", state="", country="", description="Description", temp="-273.15", feels_like="-273.15", humidity="50", wind_speed="0", weatherIcon="10d"}) => {
     const dispatch = useDispatch();
     return (
-        <Card variant="outlined" sx={{ maxWidth: 345}} >
+        <Card variant="outlined" sx={{ maxWidth: 345, height: "100%"}} >
             <Box sx={{ display: 'flex' }}>
                 <CardMedia component="img" sx={{ width: 100}} image={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt={weatherIcon} />
                 <Typography variant="h2" sx={{ padding: "20px"}}>{(temp - 273.15).toFixed(1)} °C</Typography>
             </Box>
             
-            <CardContent align="left" sx={{ paddingBottom: "0%", maxHeight: "400px" }}>
-                <ThemeProvider theme={theme}>
-                    <Typography variant="h4" gutterBottom>{city && city !== state ? city + "," : ""} {state ? state + "," : ""} {country ? country : ""}</Typography>
-                    <Typography variant="h5" gutterBottom>Feels like {(feels_like - 273.15).toFixed(1)} °C</Typography>
-                    <Typography variant="h5" gutterBottom>{description.charAt(0).toUpperCase() + description.slice(1)}</Typography>
-                    <Typography variant="h5" gutterBottom>Wind speed: {wind_speed} m/s</Typography>
-                    <Typography variant="h5">Humidity: {humidity}%</Typography>
-                </ThemeProvider>
+            <CardContent align="left" sx={{ paddingBottom: "0%"}}>
+                <Typography variant="h4" gutterBottom>{city && city !== state ? city + "," : ""} {state ? state + "," : ""} {country ? country : ""}</Typography>
+                <Typography variant="h5" gutterBottom>Feels like {(feels_like - 273.15).toFixed(1)} °C</Typography>
+                <Typography variant="h5" gutterBottom>{description.charAt(0).toUpperCase() + description.slice(1)}</Typography>
+                <Typography variant="h5" gutterBottom>Wind speed: {wind_speed} m/s</Typography>
+                <Typography variant="h5">Humidity: {humidity}%</Typography>
             </CardContent>
-            <CardActions>
+            <CardActions sx={{display: 'flex', cursor: 'pointer'}}>
                 <Button size="large" color="warning" onClick={() => dispatch(weatherRemoved(id))}>Dismiss</Button>
             </CardActions>
         </Card>
